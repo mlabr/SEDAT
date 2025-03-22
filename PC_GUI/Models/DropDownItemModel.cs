fe5dec31d8d2a7d840ffdfb0267876fbdd77587d
mlabr <mlabr@seznam.cz>
2025-03-22 23:54:21 +0100

    Add new class dropDownItemModel

---
+++ PC_GUI/Models/DropDownItemModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_GUI.Models
+{
+	internal class DropDownItemModel
+	{
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public int DbId { get; set; }
+	}
+}
