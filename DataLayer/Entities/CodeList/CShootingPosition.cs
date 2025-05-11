f214ed00e5c71bb6612d804f63189ffefd94e6e5
mlabr <mlabr@seznam.cz>
2025-05-11 22:10:50 +0200

    New database scheme preparing

--- DataLayer/Entities/CodeList/CShootingPosition.cs
+++ DataLayer/Entities/CodeList/CShootingPosition.cs
@@ -6,7 +6,7 @@ namespace DataLayer.Entities.CodeList
 	[Table("CShootingPosition")]
 	public class CShootingPosition : EntityCodeBase
 	{
-		[PrimaryKey]
+		[PrimaryKey, NotNull, AutoIncrement]
 		public int CShootingPositionId { get; set; }
 
     }
---
+++ DataLayer/Entities/ScoreSet.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+	[Table("ScoreSet")]
+	public class ScoreSet
+	{
+		[PrimaryKey, NotNull, AutoIncrement]
+		public int ScoreSetId { get; set; }
+
+		[NotNull]
+		public int RecordId { get; set; }
+
+		[NotNull]
+		public float Score { get; set; }
+
+		[NotNull]
+		public int ShotsCount { get; set; }
+	}
+}
